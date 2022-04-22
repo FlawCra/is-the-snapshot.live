985dd675bbb2eb298f36ae201b69395b12833a8c
Xanax-C-137 <moritz.fuessel@flawcra.cc>
2022-04-22 06:54:24 +0200

    added "view on" buttons

--- js/main.js
+++ js/main.js
@@ -15,8 +15,6 @@
     if(localStorage.getItem("enable-sound") == "checked") {
         localStorage.setItem("enable-sound", "");
     }
-
-    
     
     $("[name=enable-notifications]").change((el) => {
         if(Notification.permission !== "granted") {
@@ -38,9 +36,12 @@
 
     var url = "https://launchermeta.mojang.com/mc/game/version_manifest.json";
     var live_sound = "https://cdn.flawcra.cc/12/DATA/60ac5f5dae9a5c2b877cdd58988a1e32e5b5a0445a54210937fa28fa66c946f0c4591e5b0181f4d97410814d3f048cd11b15a512ae6d6dce5fa38035e3432b61/689394792700025409/snapshot_live.wav";
+    var live_sound_obj = new Audio(live_sound);
     var livetext = document.getElementById("livetext");
     var latest = document.getElementById("latest");
     var phoenix_stream = document.getElementById("phoenix_stream");
+    var buttons = document.getElementById("buttons");
+    var first_button = true;
     var latest_snapshot = "";
     var latest_snapshot_released = null;
     var not_live = `It's not live yet 😔`;
@@ -56,29 +57,23 @@
         if(FlawCraLIB.getParameterByName("snap", location.href)) {
             var snapshot = FlawCraLIB.getParameterByName("snap", location.href);
             if(await check(json, snapshot)) {
-                livetext.innerText = live_now;
-                latest.innerText = `Selected snapshot: ${snapshot}`;
-                snapshot_live_event(snapshot);
+                snapshot_event(true, snapshot);
                 clearInterval(loop);
                 return;
             } else {
-                livetext.innerText = not_live;
-                latest.innerText = `Selected snapshot: ${snapshot}`;
+                snapshot_event(false, snapshot);
                 return;
             }
             
         }
 
         if(await check(json)) {
-            livetext.innerText = live_now;
             latest_snapshot = json.latest.snapshot;
-            latest.innerText = `Latest snapshot: ${latest_snapshot}`;
-            snapshot_live_event(latest_snapshot);
+            snapshot_event(true, latest_snapshot);
             add_phoenix_stream();
             clearInterval(loop);
         } else {
-            livetext.innerText = not_live;
-            latest.innerText = `Latest snapshot: ${json.latest.snapshot}`;
+            snapshot_event(false, json.latest.snapshot);
         }
     }, 1500);
 
@@ -89,14 +84,32 @@
         }
     }
 
-    var snapshot_live_event = async (snapshot_name) => {
-        play_audio();
-        send_notification("Snapshot Live!", `The snapshot ${snapshot_name} is live!`);
+    var snapshot_event = async (live,snapshot_name) => {
+        if(live) {
+            livetext.innerText = live_now;
+            latest.innerText = `Snapshot: ${snapshot_name}`;
+            var regex = /([0-9][0-9]w[0-9][0-9])[a-z]/g
+            var match = regex.exec(snapshot_name);
+
+            if(await check_url("https://cors.flawcra.cc/?https://www.minecraft.net/en-us/article/minecraft-snapshot-"+match[1]+"a")) add_button("View on Minecraft.net", "https://www.minecraft.net/en-us/article/minecraft-snapshot-"+match[1]+"a");
+            if(await check_url("https://cors.flawcra.cc/?https://tisawesomeness.github.io/snapshots/"+match[1]+"a")) add_button("View on Tis", "https://tisawesomeness.github.io/snapshots/"+match[1]+"a");
+
+            play_audio();
+            send_notification("Snapshot Live!", `The snapshot ${snapshot_name} is live!`);
+        } else {
+            livetext.innerText = not_live;
+            latest.innerText = `Snapshot: ${snapshot_name}`;
+        }
     }
 
     var play_audio = async () => {
         if(!localStorage.getItem("enable-sound") || localStorage.getItem("enable-sound") != "checked") return;
-        new Audio(live_sound).play();
+        live_sound_obj.play();
+    }
+
+    var add_button = async (text, link) => {
+        if(!first_button) buttons.innerHTML += "<br>";
+        buttons.innerHTML += `<a class="mc-button__primary mc-button__green-s1" href="${link}" aria-label="${text}" data-aem-contentname="${text}" target="_blank">${text}</a>`
     }
 
     var send_notification = async (title, body) => {
@@ -128,6 +141,11 @@
             }
         }
         return false;
+    }
+
+    var check_url = async (url) => {
+        var request = await fetch(url);
+        return (request.status == 200) ? true : false;
     }
 
     var is_phoenix_live = async () => {
